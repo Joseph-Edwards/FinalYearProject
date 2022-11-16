950520e3bd4befc6386323c04725b90c91da8820
Joe Edwards <josephdavidedwards@gmail.com>
2022-11-16 13:34:30 +0000

    Refactor with consistent names, comments and formatting

--- Arvind.py
+++ Arvind.py
@@ -5,47 +5,104 @@ Created on Tue Oct 25 19:23:23 2022
 @author: josep
 """
 
-import networkx as nx
-from itertools import groupby, product
 from collections import deque
+from itertools import groupby, product
+import networkx as nx
+from networkx.drawing.nx_pydot import graphviz_layout
 
-X1 = nx.Graph()
-X1.add_nodes_from(range(1, 21))
-X1.add_edges_from([(6, 7),
-    (7, 8),
-    (8, 9),
-    (9, 10),
-    (10, 11),
-    (11, 12),
-    (12, 13),
-    (13, 14),
-    (14, 15),
-    (15, 9),
-    (15, 13),
-    (16, 13),
-    (17, 18),
-    (18, 19),
-    (19, 20)
-    ])
+# =============================================================================
+# Test Graphs
+# =============================================================================
 
-X2 = nx.Graph()
-X2.add_nodes_from(range(1, 7))
-X2.add_edges_from([
+# Nice looking graph from colour refinement website
+G1 = nx.Graph()
+G1.add_nodes_from(range(1, 21))
+G1.add_edges_from([(6, 7),
+                   (7, 8),
+                   (8, 9),
+                   (9, 10),
+                   (10, 11),
+                   (11, 12),
+                   (12, 13),
+                   (13, 14),
+                   (14, 15),
+                   (15, 9),
+                   (15, 13),
+                   (16, 13),
+                   (17, 18),
+                   (18, 19),
+                   (19, 20)
+                   ])
+
+# P3 on top of P5 (looks like a cross)
+G2 = nx.Graph()
+G2.add_nodes_from(range(1, 7))
+G2.add_edges_from([
     (1, 2),
     (1, 3),
     (2, 4),
     (2, 5),
     (2, 6),
     (3, 7)
-    ])
+])
+
+# K5 with K3 lollipops off each vertex
+E = [[0, 1],
+     [0, 2],
+     [1, 2],
+     [2, 3],
+     [3, 20],
+     [4, 5],
+     [4, 6],
+     [5, 6],
+     [6, 7],
+     [7, 21],
+     [8, 9],
+     [8, 10],
+     [9, 10],
+     [10, 11],
+     [11, 22],
+     [12, 13],
+     [12, 14],
+     [13, 14],
+     [14, 15],
+     [15, 23],
+     [16, 17],
+     [16, 18],
+     [17, 18],
+     [18, 19],
+     [19, 24],
+     [20, 21],
+     [20, 22],
+     [20, 23],
+     [20, 24],
+     [21, 22],
+     [21, 23],
+     [21, 24],
+     [22, 23],
+     [22, 24],
+     [23, 24]]
+G3 = nx.Graph()
+G3.add_edges_from(E)
+
+# =============================================================================
+# Colour Refinement
+# =============================================================================
+# TODO: Revisit faster CR. Don't spend too much time on it
+
+
+def first_last(array, key):
+    # Find first and last occurence of an element in a sorted list
+    return (array.index(key), len(array) - array[::-1].index(key) - 1)
+
 
 def colour_refinement(G):
     V = list(G.nodes)
-    N = {v: G[v] for v in V}
-    C = {v:1 for v in V}    # Initial colouring
-    P = {1: V}              # P associates with each colour the vertices of this colour
-    c_min, c_max = 1, 1     # Colour names are always between c_min and c_max
-    Q = deque([1])          # Contains colours used for refinement
+    N = {v: G[v] for v in V}  # Neighbours of each vertex
+    C = {v: 1 for v in V}     # Initial colouring
+    P = {1: V}                # P associates each colour with its colour class
+    c_min, c_max = 1, 1       # Colour names are always between c_min and c_max
+    Q = deque([1])            # Contains colours used for refinement
     while Q:
         q = Q.popleft()
 
@@ -54,8 +111,9 @@ def colour_refinement(G):
         D = {v: len(verts_with_col_q & set(N[v])) for v in V}
 
         # Ordered partition of vertices v sorted lexographically by (C[v], D[v])
-        list.sort(V, key = lambda v: (C[v], D[v]))
-        cols_and_parts = [(k[0], list(g))  for k, g in groupby(V, lambda v: (C[v], D[v]))]
+        list.sort(V, key=lambda v: (C[v], D[v]))
+        cols_and_parts = [(k[0], list(g))
+                          for k, g in groupby(V, lambda v: (C[v], D[v]))]
         cols_of_parts, B = list(zip(*cols_and_parts))
 
         for i in range(c_min, c_max + 1):
@@ -63,98 +121,149 @@ def colour_refinement(G):
             k1, k2 = first_last(cols_of_parts, i)
 
             # Add all colours, except the one with the largest class, to Q
-            i_star = max(range(k1, k2 + 1), key = lambda i: len(B[i]))
+            i_star = max(range(k1, k2 + 1), key=lambda i: len(B[i]))
             for i in range(k1, k2 + 1):
                 if i != i_star:
                     Q.append(c_max + i + 1)
 
         c_min, c_max = c_max + 1, c_max + len(B)    # New colour range
         for b in range(c_min, c_max + 1):
-            B_current =  B[b - c_min]
+            B_current = B[b - c_min]
             P[b] = B_current                        # New colouring
             for v in B_current:
                 C[v] = b
     return C, P
 
-def first_last(array,key):
-    return (array.index(key), len(array) - array[::-1].index(key) - 1)
-
-def augmented_cell_graph(G):
-    # Get stable colouring
-    C, P = colour_refinement(G)
-    stable_colouring = {v: P[v] for v in set(C.values())}
-
-    # Vertices of C* are colour classes of (G, C)
-    V = stable_colouring.keys()
-
-    # Find the adjacency list and d_ijs
-    adj_list = {}   # vertex: [neighbours]
-    d_ij = {}       # (i, j): d_ij
-    for u in V:
-        edges, ds = find_edges(stable_colouring[u], G, C)
-        adj_list[u] = edges
-        for colour, d in ds.items():
-            d_ij[(u, colour)] = d
-
-    # Convert vertex list and adjacency list to nx Graph
-    aug_G = nx.Graph()
-    aug_G.add_nodes_from(V)
-    for u, nbs in adj_list.items():
-        aug_G.add_edges_from(product([u], nbs))
-    return aug_G, d_ij, stable_colouring
+# =============================================================================
+# Create Augmented Cell Graph
+# =============================================================================
 
 
-def find_edges(X, G, C):
+def find_edges(col_class, G, C):
     # Get representative
-    u = X[0]
+    u = col_class[0]
 
-    # Get the colour class of each neighbour
+    # Get the colour of each neighbour
     adjacent_colours = [C[v] for v in G[u]]
 
     unique_colours = set(adjacent_colours)
 
     # Count the number of occurences of each neighbour
-    d_ij = {C[u]:0}
-    d_ij |= {i:adjacent_colours.count(i) for i in unique_colours}
+    d_ij = {C[u]: 0}
+    d_ij |= {i: adjacent_colours.count(i) for i in unique_colours}
 
-    adj_list = unique_colours - {C[u]} # Can there be loops?
+    adj_list = unique_colours - {C[u]}  # Can there be loops?
     return adj_list, d_ij
 
-#%%
 
-def is_amenable(G):
-    aug_G, d_ij, C = augmented_cell_graph(G)
-    V = list(aug_G.nodes)
-    cell_size = {v: len(C[v]) for v in V} # Make consistent names
+def augmented_cell_graph(G):
+    # Get stable colouring
+    C, P = colour_refinement(G)
+    stable_col_classes = {v: P[v] for v in set(C.values())}
 
-    for u, v in d_ij.keys():
-        d = d_ij[(u, v)]
-        if u == v:
-            # Check A
-            u_size = cell_size[u]
-            # Empty, Kn, mK2, complement of mK2, C5
-            if not (d in (0, u_size - 1, 1, u_size - 2) or (d == 2 and u_size == 5)):
-                return False
-        else:
-            # Check B
-            u_size = cell_size[u]
-            v_size = cell_size[v]
-            # Empty, Kmn, sK1t, sKt1, complement of sK1t, complement of sKt1
-            if not (d in (0, v_size, 1, v_size/u_size, v_size - 1, v_size - v_size/u_size)):
-                return False
-    try:
-        comps = anisotropic_components(aug_G, d_ij, cell_size)
-    except AssertionError:
-        print("Fails H")
-        return False
+    # Vertices of C* are colours used in (G, C)
+    aug_V = stable_col_classes.keys()
 
-    for comp, root in comps:
-        if not satisfies_monotonicity(comp, root, cell_size):
-            print("Fails G")
+    # Make the adjacency list of  aug_G and find d_ijs
+    adj_list = {}   # vertex: [neighbours]
+    d_ij = {}       # (i, j): d_ij
+    for aug_u in aug_V:
+        edges, ds = find_edges(stable_col_classes[aug_u], G, C)
+        adj_list[aug_u] = edges
+        for aug_v, d in ds.items():
+            d_ij[(aug_u, aug_v)] = d
+
+    # Convert vertex list and adjacency list to nx Graph
+    aug_G = nx.Graph()
+    aug_G.add_nodes_from(aug_V)
+    for aug_u, nbs in adj_list.items():
+        aug_G.add_edges_from(product([aug_u], nbs))
+    return aug_G, d_ij, stable_col_classes
+
+# =============================================================================
+# Find Valid Anisotropic Components
+# =============================================================================
+# TODO: Add definitions
+
+
+def is_anisotropic(u, v, d, cell_size):
+    return d[(u, v)] not in (0, cell_size[v])
+
+
+def is_heterogeneous(u, d, cell_size):
+    return d[(u, u)] not in (0, cell_size[u] - 1)
+
+
+def anisotropic_components(aug_G, d, cell_size):
+    aug_V = set(aug_G.nodes)
+    components = []
+
+    # Iterate until we have visited all nodes in aug_G
+    while aug_V:
+        # Use breadth first search to build components
+        root = aug_V.pop()
+        visit = deque([root])
+        seen = {root}
+        component = nx.Graph()
+
+        min_card = cell_size[root]
+        min_card_vertex = root
+
+        found_het = False
+
+        while visit:
+            aug_u = visit.popleft()
+            card = cell_size[aug_u]
+            # TODO: Put this into seperate function
+            # Check H
+            if is_heterogeneous(aug_u, d, cell_size):
+                if (not found_het) and card <= min_card:
+                    found_het = True
+                    het_card = card
+                else:
+                    raise AssertionError  # TODO: Make a new assertion class
+            elif found_het and card < het_card:
+                raise AssertionError
+
+            # Maintain record of minimum cardinality vertex
+            if card < min_card:
+                min_card = card
+                min_card_vertex = aug_u
+
+            component.add_node(aug_u)
+
+            # Search Neighbours and add edge to component if anisotropic
+            for aug_v in aug_G[aug_u]:
+                if is_anisotropic(aug_u, aug_v, d, cell_size):
+                    component.add_edge(aug_u, aug_v)
+                    if aug_v not in seen:
+                        visit.append(aug_v)
+                        seen |= {aug_v}
+                        aug_V.remove(aug_v)
+
+        # Check component is Tree (Part of G)
+        assert nx.is_tree(component)
+        components.append((component, min_card_vertex))
+    return components
+
+# =============================================================================
+# Check Monotonicity
+# =============================================================================
+
+
+def satisfies_monotonicity(component, root, cell_size):
+    for aug_u, aug_v in bfs(component, root):
+        if cell_size[aug_u] > cell_size[aug_v]:
             return False
     return True
 
+# =============================================================================
+# Amenability Testiing
+# =============================================================================
+
+
 def bfs(G, root):
+    # Breadth First Search over a tree
     visit = deque([root])
     visited = set()
     while visit:
@@ -163,66 +272,79 @@ def bfs(G, root):
         for v in G[u]:
             if v not in visited:
                 visit.append(v)
-                yield (u,v)
+                yield (u, v)
 
-def anisotropic_components(aug_G, d, cell_size):
-    V = set(aug_G.nodes)
-    components = []
-    while V:
-        root = V.pop()
-        visit = deque([root])
-        seen = {root}
-        component = nx.Graph()
 
-        min_card = cell_size[root]
-        min_card_v = root
+def is_amenable(G):
+    aug_G, d_ij, col_classes = augmented_cell_graph(G)
+    aug_V = list(aug_G.nodes)
+    cell_size = {aug_v: len(col_classes[aug_v]) for aug_v in aug_V}
 
-        found_het = False
+    for aug_u, aug_v in d_ij.keys():
+        d = d_ij[(aug_u, aug_v)]
+        if aug_u == aug_v:
+            # Check A
+            aug_u_size = cell_size[aug_u]
+            if not (
+                d in (
+                    0,                           # Empty
+                    aug_u_size - 1,              # Kn
+                    1,                           # mK2
+                    aug_u_size - 2               # Complement of mK2
+                )
+                or (d == 2 and aug_u_size == 5)  # C5
+            ):
+                print("Fails A")
+                return False
+        else:
+            # Check B
+            aug_u_size = cell_size[aug_u]
+            aug_v_size = cell_size[aug_v]
+            if not (d in (
+                0,                                  # Empty
+                aug_v_size,                         # Kmn
+                1,                                  # sK1t
+                aug_v_size/aug_u_size,              # sKt1
+                aug_v_size - 1,                     # Complement of sK1t
+                aug_v_size - aug_v_size/aug_u_size  # Complement of sKt1
+            )):
+                print("Fails B")
+                return False
 
-        while visit:
-            u = visit.popleft()
-            card = cell_size[u]
-            if is_heterogeneous(u, d, cell_size):
-                if (not found_het) and card <= min_card:
-                    found_het = True
-                    het_card = card
-                else:
-                    raise AssertionError
-            elif found_het and card < het_card:
-                raise AssertionError
+    # Get anisotropic components and Check H
+    try:
+        comps = anisotropic_components(aug_G, d_ij, cell_size)
+    except AssertionError:
+        print("Fails H")
+        return False
 
-            if card < min_card:
-                min_card = card
-                min_card_v = u
-
-            component.add_node(u)
-            for v in aug_G[u]:
-                if is_anisotropic(u, v, d, cell_size):
-                    component.add_edge(u,v)
-                    if v not in seen:
-                        visit.append(v)
-                        seen |= {v}
-                        V.remove(v)
-        assert(nx.is_tree(component))
-        components.append((component, min_card_v))
-    return components
-
-def satisfies_monotonicity(comp, root, cell_size):
-    for u, v in bfs(comp, root):
-        if cell_size[u] > cell_size[v]:
+    # Test monotonicity (Part of G)
+    for comp, root in comps:
+        if not satisfies_monotonicity(comp, root, cell_size):
+            print("Fails G")
             return False
     return True
 
-def is_anisotropic(u, v, d, cell_size):
-    return d[(u,v)] not in (0, cell_size[v])
+# =============================================================================
+# Testting
+# =============================================================================
 
-def is_heterogeneous(u, d, cell_size):
-    return d[(u, u)] not in (0, cell_size[u] - 1)
 
-# list(bfs(nx.cycle_graph(6), range(6)))
-is_amenable(nx.barbell_graph(3, 1))
-#c, p = colour_refinement(X1)
-#nx.draw_circular(X1, node_color = list(c.values()), labels = c)
-#aug_G, ds, C = augmented_cell_graph(X1)
-#nx.draw(aug_G, with_labels = True)
+test_graph = G3
 
+Cols, _ = colour_refinement(test_graph)
+# https://graphviz.org/docs/layouts/
+pos = graphviz_layout(test_graph, prog="circo")
+nx.draw(test_graph, pos=pos, with_labels=True, node_color=list(Cols.values()))
+
+is_amenable(test_graph)
+
+# =============================================================================
+# Other notes
+# =============================================================================
+# TODO: Consider making some variables global
+# TODO: Profile. Consider removing nx
+# TODO: Make speed plots
+# TODO: Throw fun graphs at it
+
+# TODO (Bonus): Colour anisotropic components
